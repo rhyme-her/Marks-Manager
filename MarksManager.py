5d7d4b282222f55a771c7c98b3641ab39081998b
Rhyme <rhyme.her@hotmail.com>
2022-05-17 19:43:44 -0400

    Created README.md.

--- MarksManager.py
+++ MarksManager.py
@@ -3,24 +3,6 @@
 # by Rhyme Her
 # Created: 2017-01-27
 
-# STATUS: Not Completed
-    # Fix: Displaying the category names.
-        # I perceived them as the last valuable element, leaving it for last.
-    # Learn how to add scroll bars.
-        # Couldn't learn to draw through Tkinter, a new challenge for next time!
-    # Delete button.
-
-# Issues:
-    # It always appeared that I would get the wrong averages.
-        # However, not using the examples and instead my own answers gave the correct ones.
-    # Renaming the classroom creates a new file.
-        #Since there is currently no delete button, new files that are created do not delete their old variants.
-
-# Files:
-    # I left an example file called: "Classroom.txt". It's essentially a copy of the examples
-    # given, so any other example could be used.
-
-
 #Procedure
 
 #1. Start with getting a small window through graphics.
